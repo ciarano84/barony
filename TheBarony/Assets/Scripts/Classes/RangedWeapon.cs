9618e089aaee1f5dec2e2d4dd7dab4d6515d4aac
Ciaran O'Connor <ciarano84@gmail.com>
2020-06-08 10:33:39 +0100

    RangedWeapons 3
    Scouts will find all viable targets and store them in the target's list.

--- TheBarony/Assets/Scripts/Classes/RangedWeapon.cs
+++ TheBarony/Assets/Scripts/Classes/RangedWeapon.cs
@@ -7,7 +7,7 @@ public class RangedWeaponData : WeaponData
     public override void SetWeaponData()
     {
         imageFile = "Shortbow";
-        range = 20;
+        range = 200;
     }
 
     public override void CreateWeapon(Unit unit)
@@ -21,7 +21,6 @@ public class RangedWeaponData : WeaponData
 
 public class RangedWeapon : Weapon
 {
-
     public override IEnumerator Attack(Target target)
     {
         yield break;
@@ -30,19 +29,30 @@ public class RangedWeapon : Weapon
     public override void GetTargets()
     {
         targets.Clear();
-
         foreach (TacticsMovement unit in Initiative.order)
         {
             if (unit != owner.GetComponent<TacticsMovement>())
             {
-                Vector3 rayOrigin = owner.transform.position;
+                Vector3 rayOrigin = owner.gameObject.transform.position;
+                Vector3 centre = -Vector3.up;
+
+                //Debug
+                Debug.DrawRay(transform.position, (unit.gameObject.transform.position - transform.position), Color.green, 1000f);
+
                 // Declare a raycast hit to store information about what our raycast has hit
-                RaycastHit hit;
-                if (Physics.Raycast(rayOrigin, owner.transform.forward, out hit, owner.weapon1.weaponData.range))
-                { 
-                    
+                if (Physics.Raycast(rayOrigin, (unit.gameObject.transform.position - transform.position), out RaycastHit hit))
+                {
+                    if (unit == hit.collider.gameObject.GetComponent<TacticsMovement>())
+                    {
+                        AddTarget(unit, owner.currentTile); ;
+                    }
                 }
             }
+        }
+
+        foreach (Target t in targets)
+        {
+            Debug.Log(t.unitTargeted.gameObject);
         }
     }
 }
