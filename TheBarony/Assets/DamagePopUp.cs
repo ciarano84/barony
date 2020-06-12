5f68a642d42553b6010ba5bd297ea960c442837a
Ciaran O'Connor <ciarano84@gmail.com>
2020-06-13 00:13:05 +0100

    DamagePopUps 1
    started work

---
+++ TheBarony/Assets/DamagePopUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DamagePopUp : MonoBehaviour
+{
+    TextMeshPro textMesh;
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMeshPro>();
+    }
+
+
+    /*public static DamagePopUp Create(Vector3 position, int damageAmount)
+    {
+        Debug.Log("called");
+        //Transform damagePopUpTransform = Instantiate(TestTriggerCombatPopUp.pfDamagePopup, position, Quaternion.identity);
+        //DamagePopUp damagePopUp = damagePopUpTransform.GetComponent<DamagePopUp>();
+        //damagePopUp.Setup(damageAmount);
+
+        return damagePopUp;
+    }*/
+
+
+
+    public void Setup(int damageAmount)
+    {
+        textMesh.SetText(damageAmount.ToString());
+    }
+}
---
+++ TheBarony/Assets/Scripts/Debugging Scripts/TestTriggerCombatPopUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTriggerCombatPopUp : MonoBehaviour
+{
+    public Transform pfDamagePopup;
+    public GameObject target;
+
+    /*private void Start()
+    {
+        DamagePopUp.Create(target.transform.position, 600);
+    }*/
+}
