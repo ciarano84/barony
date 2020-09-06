e3a821f14e5c9f61998879bf8218669b126dc64c
Ciaran O'Connor <ciarano84@gmail.com>
2020-09-06 12:59:35 +0100

    arenas-10
    blocks have random rotation when placed.

--- TheBarony/Assets/Scripts/ArenaPoint.cs
+++ TheBarony/Assets/Scripts/ArenaPoint.cs
@@ -5,4 +5,28 @@ using UnityEngine;
 public class ArenaPoint : MonoBehaviour
 {
     public GameObject block;
+
+    private void Start()
+    {
+        int rotationRoll = Random.Range(0, 4);
+        float rotation;
+
+        switch (rotationRoll)
+        {
+            case 1:
+                rotation = 90f;
+                break;
+            case 2:
+                rotation = 180f;
+                break;
+            case 3:
+                rotation = 270f;
+                break;
+            default:
+                rotation = 0;
+                break;
+        }
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, rotation, transform.eulerAngles.z);
+    }
 }
