9036f798b32565cbf4385740f864bde971078d7b
Ciaran O'Connor <ciarano84@gmail.com>
2020-10-03 17:41:06 +0100

    combat-style-1
    pause function included.

---
+++ TheBarony/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool paused = false;
+    public static bool actionPaused = false;
+
+    public void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            PauseGame();
+        }
+
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            PauseAction();
+        }
+    }
+
+    public void PauseGame()
+    {
+        paused = !paused;
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public static void PauseAction()
+    {
+        actionPaused = !actionPaused;
+
+        //Unit unit = Initiative.currentUnit.GetComponent<Unit>();
+
+        if (actionPaused)
+        {
+            foreach (TacticsMovement t in Initiative.order)
+            {
+                Unit unit = t.GetComponent<Unit>();
+                unit.enabled = false;
+                unit.unitAnim.enabled = false;
+            } 
+        }
+        else if (!actionPaused)
+        {
+            foreach (TacticsMovement t in Initiative.order)
+            {
+                Unit unit = t.GetComponent<Unit>();
+                unit.enabled = true;
+                unit.unitAnim.enabled = true;
+            }
+        }
+    }
+}
