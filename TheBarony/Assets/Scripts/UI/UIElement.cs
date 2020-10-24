55753a0a60629ecb6ca69c366a2b6009f2496a3d
Ciaran O'Connor <ciarano84@gmail.com>
2020-10-24 17:02:37 +0100

    combat-style-8
    made tooltip work on the defence toggle.

---
+++ TheBarony/Assets/Scripts/UI/UIElement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIElement : MonoBehaviour
+{
+    public string tooltipText;
+    Tooltip tooltip;
+    ActionUIManager UIManager;
+
+    public void Awake()
+    {
+        UIManager = GameObject.Find("Main Action Panel").GetComponent<ActionUIManager>();
+        tooltip = UIManager.tooltip.GetComponent<Tooltip>();
+    }
+
+    public void ShowTooltip()
+    {
+        tooltip.ShowToolTip(tooltipText);
+    }
+
+    public void HideTootip()
+    {
+        tooltip.HideToolTip();
+    }
+
+    public void RemoveSelf()
+    {
+        tooltip.HideToolTip();
+        Destroy(gameObject);
+    }
+}
